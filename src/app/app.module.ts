b57e7b5dcae2d52d71436b2b3eb462e444aded43
Oliver <oliver@yezz.com.tw>
2020-09-14 10:54:04 +0800

    feat: 建立 Task 模組

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskModule } from './task/task.module';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, TaskModule],
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
