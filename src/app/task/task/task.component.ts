25b1aa64d467c16ca8d7004f067bd971cff7f908
Oliver <oliver@yezz.com.tw>
2020-09-16 23:42:29 +0800

    feat: 單向資料繫結 - 利用內嵌繫結實作顯示待辦事項主旨

--- src/app/task/task/task.component.ts
+++ src/app/task/task/task.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskState } from '../../enum/task-state.enum';
+import { Task } from '../../model/task';
+
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: ['./task.component.css'],
 })
 export class TaskComponent implements OnInit {
+  task: Task;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.task = new Task('頁面需要顯示待辦事項主旨');
   }
 
+  getStateDesc(): string {
+    switch (this.task.state) {
+      case TaskState.None:
+        return '未完成';
+      case TaskState.Doing:
+        return '進行中';
+      case TaskState.Finish:
+        return '已完成';
+    }
+  }
 }
