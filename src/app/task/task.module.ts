33efe4a04cdab916209ad35a2c6928380f7813d8
Oliver <oliver@yezz.com.tw>
2020-09-23 00:19:47 +0800

    feat: 屬性型指令 - 利用屬性型指令實作狀態文字變更

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskComponent } from './task/task.component';
+import { TaskStateColorDirective } from './task-state-color.directive';
 
 
 
 @NgModule({
-  declarations: [TaskComponent],
+  declarations: [TaskComponent, TaskStateColorDirective],
   imports: [
     CommonModule
   ],
