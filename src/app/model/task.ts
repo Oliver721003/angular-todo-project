812b19ad66d8aa3216176b41cdc81aa5d9d194d9
Oliver <oliver@yezz.com.tw>
2020-09-25 14:44:59 +0800

    feat: 結構型指令 ngSwitch - 前置作業

--- src/app/model/task.ts
+++ src/app/model/task.ts
@@ -2,4 +2,10 @@ import { TaskState } from '../enum/task-state.enum';
 
 export class Task {
   constructor(public subject: string, public state: TaskState = TaskState.None) {}
+
+  level?: 'XS' | 'S' | 'M' | 'L' | 'XL';
+
+  expectDate?: Date;
+
+  finishedDate?: Date;
 }
