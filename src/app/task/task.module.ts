fa9326f314027709e0a0c5aac0e66612a77f3803
Oliver <oliver@yezz.com.tw>
2020-09-29 01:07:07 +0800

    feat: 管道 - 自訂管道實作民國年日期顯示

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TaskComponent } from './task/task.component';
 import { TaskStateColorDirective } from './task-state-color.directive';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaiwanDatePipe } from './taiwan-date.pipe';
 
 
 
 @NgModule({
-  declarations: [TaskComponent, TaskStateColorDirective, TaskListComponent],
+  declarations: [TaskComponent, TaskStateColorDirective, TaskListComponent, TaiwanDatePipe],
   imports: [
     CommonModule
   ],
