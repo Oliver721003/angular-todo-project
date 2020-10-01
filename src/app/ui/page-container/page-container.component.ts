52dc0b2f8e033860628bb18a812a9db50677346b
Oliver <oliver@yezz.com.tw>
2020-10-01 15:24:18 +0800

    feat: ngContent - 生命週期

--- src/app/ui/page-container/page-container.component.ts
+++ src/app/ui/page-container/page-container.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, DoCheck, OnInit } from '@angular/core';
+
+import { PageTitleComponent } from '../page-title/page-title.component';
 
 @Component({
   selector: 'app-page-container',
   templateUrl: './page-container.component.html',
-  styleUrls: ['./page-container.component.css']
+  styleUrls: ['./page-container.component.css'],
 })
-export class PageContainerComponent implements OnInit {
+export class PageContainerComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked {
+  @ContentChild(PageTitleComponent) title: HTMLElement;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log('PageContainerComponent - ngOnInit');
   }
 
+  ngDoCheck(): void {
+    console.log('PageContainerComponent - ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('PageContainerComponent - ngAfterContentInit', this.title);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('PageContainerComponent - ngAfterContentChecked');
+  }
 }
