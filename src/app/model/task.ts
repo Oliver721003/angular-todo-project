9ef3081984631f215b3216ce8ccf85744f8ec944
Oliver <oliver@yezz.com.tw>
2020-10-04 15:56:51 +0800

    feat: HttpClient - 前置作業

--- src/app/model/task.ts
+++ src/app/model/task.ts
@@ -1,7 +1,7 @@
 import { TaskState } from '../enum/task-state.enum';
 
 export class Task {
-  constructor(public subject: string, public state: TaskState = TaskState.None) {}
+  constructor(public id: number, public subject: string, public state: TaskState = TaskState.None) {}
 
   level?: 'XS' | 'S' | 'M' | 'L' | 'XL';
 
--- src/app/task/services/task-local.service.ts
+++ src/app/task/services/task-local.service.ts
@@ -11,9 +11,9 @@ export class TaskLocalService {
 
   constructor() {
     this._tasks = [
-      new Task('頁面需要顯示待辦事項主旨'),
-      new Task('可以設定待辦事項的狀態', TaskState.Doing),
-      new Task('當待辦事項狀態為已完的事項無法編輯事項', TaskState.Finish),
+      new Task(1, '頁面需要顯示待辦事項主旨'),
+      new Task(2, '可以設定待辦事項的狀態', TaskState.Doing),
+      new Task(3, '當待辦事項狀態為已完的事項無法編輯事項', TaskState.Finish),
     ];
     this._tasks[0].level = 'XS';
     this._tasks[0].tags = ['FEATURE', 'ISSUE', 'enhancement', 'discussion'];
