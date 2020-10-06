942fb25afa9bc29ede04dbb9079baf500bb5c96f
Oliver <oliver@yezz.com.tw>
2020-10-06 11:11:25 +0800

    feat: 依賴注入 - 使用 useValue 抽換待辦事項服務

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskLocalService } from './task/services/task-local.service';
 import { TaskRemoteService } from './task/services/task-remote.service';
 import { TaskModule } from './task/task.module';
 import { UiModule } from './ui/ui.module';
@@ -12,7 +12,17 @@ import { UiModule } from './ui/ui.module';
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, TaskModule, UiModule],
   declarations: [AppComponent],
-  providers: [{ provide: TaskRemoteService, useClass: TaskLocalService }],
+  providers: [
+    {
+      provide: TaskRemoteService,
+      useValue: {
+        getData: () => {
+          console.log('from value provider');
+          return of([]);
+        },
+      },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
