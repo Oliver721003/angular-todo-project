ec1cbc116e4c064c88fccfef1f21135657c7f638
Oliver <oliver@yezz.com.tw>
2020-10-06 11:12:32 +0800

    feat: 依賴注入 - 利用 useExisting 抽換待辦事項服務

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { of } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskLocalService } from './task/services/task-local.service';
 import { TaskRemoteService } from './task/services/task-remote.service';
 import { TaskModule } from './task/task.module';
 import { UiModule } from './ui/ui.module';
@@ -12,17 +12,7 @@ import { UiModule } from './ui/ui.module';
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, TaskModule, UiModule],
   declarations: [AppComponent],
-  providers: [
-    {
-      provide: TaskRemoteService,
-      useValue: {
-        getData: () => {
-          console.log('from value provider');
-          return of([]);
-        },
-      },
-    },
-  ],
+  providers: [TaskLocalService, { provide: TaskRemoteService, useExisting: TaskLocalService }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
