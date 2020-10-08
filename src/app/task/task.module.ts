eb6c59dd9e8548d7fb9775f68301dcab4bb10baf
Oliver <oliver@yezz.com.tw>
2020-10-08 16:57:08 +0800

    feat: RF - 前置作業

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -7,10 +7,11 @@ import { TaiwanDatePipe } from './taiwan-date.pipe';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskStateColorDirective } from './task-state-color.directive';
 import { TaskComponent } from './task/task.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule, FormsModule],
-  declarations: [TaskComponent, TaskStateColorDirective, TaskListComponent, TaiwanDatePipe],
-  exports: [TaskComponent, TaskListComponent],
+  declarations: [TaskComponent, TaskStateColorDirective, TaskListComponent, TaiwanDatePipe, TaskFormComponent],
+  exports: [TaskComponent, TaskListComponent, TaskFormComponent],
 })
 export class TaskModule {}
