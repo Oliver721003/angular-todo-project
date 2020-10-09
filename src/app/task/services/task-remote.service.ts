f8abbedcf6011ea24acf38c83bcbd0e4e2dfec72
Oliver <oliver@yezz.com.tw>
2020-10-09 19:25:24 +0800

    feat: RF - 利用 FormArray 實作標籤輸入

--- src/app/task/services/task-remote.service.ts
+++ src/app/task/services/task-remote.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { Task } from '../../model/task';
 
@@ -9,6 +10,8 @@ import { Task } from '../../model/task';
 })
 export class TaskRemoteService {
   private _url = 'http://localhost:3000/tasks';
+
+  private _id: number;
 
   constructor(private httpClient: HttpClient) {}
 
@@ -21,6 +24,12 @@ export class TaskRemoteService {
       condition.push(`state=${state}`);
     }
     const url = this._url + (condition.length === 0 ? '' : `?${condition.join('&')}`);
-    return this.httpClient.get<Task[]>(url);
+    return this.httpClient.get<Task[]>(url).pipe(tap((tasks) => (this._id = Math.max(...tasks.map((task) => task.id)) + 1)));
+  }
+
+  add(task: Task): Observable<any> {
+    task.id = this._id;
+    this._id++;
+    return this.httpClient.post(this._url, task);
   }
 }
--- src/app/task/task-form/task-form.component.ts
+++ src/app/task/task-form/task-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+import { TaskRemoteService } from '../services/task-remote.service';
 
 @Component({
   selector: 'app-task-form',
@@ -9,20 +11,34 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TaskFormComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  get tags(): FormArray {
+    return this.form.get('tags') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder, private taskService: TaskRemoteService) {}
 
   ngOnInit(): void {
-    // this.form = this.fb.group({
-    //   subject: this.fb.control(undefined),
-    //   state: this.fb.control(0),
-    //   level: this.fb.control(undefined),
-    // });
-
-    // FormBuilder 縮寫
     this.form = this.fb.group({
-      subject: [],
-      state: [0],
-      level: [],
+      subject: this.fb.control(undefined),
+      state: this.fb.control(0),
+      level: this.fb.control(undefined),
+      tags: this.fb.array([]),
     });
+  }
+
+  onAddTag(): void {
+    // const tag = this.fb.group({
+    //   tag: this.fb.control(undefined),
+    // });
+    const tag = this.fb.control(undefined);
+    this.tags.push(tag);
+  }
+
+  onDeleteTag(index: number): void {
+    this.tags.removeAt(index);
+  }
+
+  onSave(): void {
+    this.taskService.add(this.form.value).subscribe(() => console.log('save'));
   }
 }
